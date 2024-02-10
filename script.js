fecd6d93833c5b5b07c7bd45e011176c331f6cd3
Harsh Tyagi <69513043+harshtyagi-code@users.noreply.github.com>
2024-02-10 12:46:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -35,40 +35,4 @@ class Square extends Rectangle  {
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
-window.Square = Square;class Rectangle {
-	constructor(width ,height)
-	{
-		this.width=width;
-		this.height=height;
-	}
-
-	get getWidth()
-	{
-		return this.width;
-	}
-
-	get getHeight()
-	{
-		return this.height;
-	}
-
-	getArea()
-	{
-		return (this.width) * (this.height);
-	}
-}
-
-class Square extends Rectangle  {
-	constructor(width)
-	{
-		super(width,width);
-	}
-	getPerimeter()
-	{
-		return 4*(this.width);
-	}
-}
-
-// Do not change the code below this line
-window.Rectangle = Rectangle;
 window.Square = Square;
